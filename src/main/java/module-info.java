c3ada825679f0f44adca9a76ff525dba6fd89ffa
Gerrit Grunwald <han.solo@mac.com>
2022-03-08 15:59:09 +0100

    updated dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -15,6 +15,11 @@ module eu.hansolo.fx.countries {
     requires transitive eu.hansolo.toolboxfx;
     requires transitive eu.hansolo.fx.heatmap;
 
+    opens eu.hansolo.fx.countries to eu.hansolo.toolbox, eu.hansolo.toolboxfx, eu.hansolo.fx.heatmap;
+    opens eu.hansolo.fx.countries.evt to eu.hansolo.toolbox, eu.hansolo.toolboxfx, eu.hansolo.fx.heatmap;
+    opens eu.hansolo.fx.countries.flag to eu.hansolo.toolbox, eu.hansolo.toolboxfx, eu.hansolo.fx.heatmap;
+    opens eu.hansolo.fx.countries.tools to eu.hansolo.toolbox, eu.hansolo.toolboxfx, eu.hansolo.fx.heatmap;
+
     exports eu.hansolo.fx.countries;
     exports eu.hansolo.fx.countries.evt;
     exports eu.hansolo.fx.countries.flag;

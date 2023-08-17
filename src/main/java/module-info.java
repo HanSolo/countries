c0024e6695ceb40790342fcd35b5bea3dc71f3fe
samypr100 <3933065+samypr100@users.noreply.github.com>
2023-08-17 13:54:10 -0400

    feat: streamline dependency/module graph

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,20 +5,18 @@ module eu.hansolo.fx.countries {
     requires java.desktop;
 
     // Java-FX
-    requires transitive javafx.base;
-    requires transitive javafx.graphics;
-    requires transitive javafx.controls;
-    requires transitive javafx.swing;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.swing;
 
     // 3rd Party
-    requires transitive eu.hansolo.toolbox;
-    requires transitive eu.hansolo.toolboxfx;
     requires transitive eu.hansolo.fx.heatmap;
 
-    opens eu.hansolo.fx.countries to eu.hansolo.toolbox, eu.hansolo.toolboxfx, eu.hansolo.fx.heatmap;
-    opens eu.hansolo.fx.countries.evt to eu.hansolo.toolbox, eu.hansolo.toolboxfx, eu.hansolo.fx.heatmap;
-    opens eu.hansolo.fx.countries.flag to eu.hansolo.toolbox, eu.hansolo.toolboxfx, eu.hansolo.fx.heatmap;
-    opens eu.hansolo.fx.countries.tools to eu.hansolo.toolbox, eu.hansolo.toolboxfx, eu.hansolo.fx.heatmap;
+    opens eu.hansolo.fx.countries to eu.hansolo.fx.heatmap;
+    opens eu.hansolo.fx.countries.evt to eu.hansolo.fx.heatmap;
+    opens eu.hansolo.fx.countries.flag to eu.hansolo.fx.heatmap;
+    opens eu.hansolo.fx.countries.tools to eu.hansolo.fx.heatmap;
 
     exports eu.hansolo.fx.countries;
     exports eu.hansolo.fx.countries.evt;
